d9febf1993870c3afa8c359de90ce31b8709a944
Rebin Albazi <rebin.albazi@hotmail.de>
2022-03-13 01:07:07 +0100

    + add delete game function + add scrollviewer for game description + bug fixes

--- Games-Library/MainWindow.xaml.cs
+++ Games-Library/MainWindow.xaml.cs
@@ -1,57 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Media.Imaging;
-using System.Text;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace Games_Library
 {
     public partial class MainWindow : Window
     {
-        public GridViewColumnHeader listViewSortCol = null;
-        public SortAdorner listViewSortAdorner = null;
+        private GridViewColumnHeader listViewSortCol = null;
+        private SortAdorner listViewSortAdorner = null;
 
         public MainWindow()
         {
             InitializeComponent();
 
-            // Lädt zum Start des Programmes die 00_All _Games.csv
+            /// Lädt zum Start des Programmes die 00_All _Games.csv
             LoadLibrary("00_All _Games");
 
-            // Lädt zum Start alle vorhandenen Listen in die Combobox Items
+            /// Lädt zum Start alle vorhandenen Listen in die Combobox Items
             CreateComboBoxListItems();
 
-            // Release-Jahr items werden für die Combobox releaseYearFilter von 1990-2022 angelegt
-            for (int i = 2022; i > 1989; i--)
-            {
-                ComboBoxItem item = new ComboBoxItem();
-                item.Content = i;
-                releaseYearFilter.Items.Add(item);
-            }
+            /// ReleaseYearItems werden der Combobox releaseYearFilter von 1990-2022 hinzugefügt
+            CreateReleaseYearList(1990,2022);
         }
         public IEnumerable<Game> ReadCSV(string fileName)
         {
-            // Für die Auflistung wird die Variable lines als string array deklariert und kann nur .csv dateien einlesen
+            /// Für die Auflistung wird die Variable lines als string array deklariert und kann nur .csv dateien einlesen
             string[] lines = File.ReadAllLines(Path.ChangeExtension(fileName, ".csv"));
 
-            // lines.Select erlaubt, jede Zeile als Spiel zu wiedergeben.
-            // Diese gibt dann ein IEnumerable<Game> zurück.
+            /// lines.Select erlaubt, jede Zeile als Spiel zu wiedergeben.
+            /// Diese gibt dann ein IEnumerable<Game> zurück.
             return lines.Select(line =>
             {
-                // Nach jedem Semikolin werden die Daten in der .csv-Datei getrennt
+                /// Nach jedem Semikolin werden die Daten in der .csv-Datei getrennt
                 string[] data = line.Split(';');
-                // Das Spiel wird dann mit einzelnen Daten in der richtigen Reihenfolge zurückgegeben.
+                /// Das Spiel wird dann mit einzelnen Daten in der richtigen Reihenfolge zurückgegeben.
                 return new Game(data[0], data[1], data[2], data[3], data[4], Convert.ToInt32(data[5]), data[6]);
             });
         }
 
-        // Sortier-Methode beim rauf klicken der jeweiligen Spiele-Eigenschaft
+        /// Sortier-Methode beim rauf klicken der jeweiligen Spiele-Eigenschaft
         private void SortColumnHeader_Click(object sender, RoutedEventArgs e)
         {
             GridViewColumnHeader column = (sender as GridViewColumnHeader);
@@ -62,10 +59,10 @@ namespace Games_Library
                 AdornerLayer.GetAdornerLayer(listViewSortCol).Remove(listViewSortAdorner);
                 ListViewGame.Items.SortDescriptions.Clear();
             }
-            // Aufsteigende Sortierung
+            /// Aufsteigende Sortierung
             ListSortDirection newDir = ListSortDirection.Ascending;
 
-            // Absteigende Sortierung
+            /// Absteigende Sortierung
             if (listViewSortCol == column && listViewSortAdorner.Direction == newDir)
                 newDir = ListSortDirection.Descending;
 
@@ -75,50 +72,52 @@ namespace Games_Library
             ListViewGame.Items.SortDescriptions.Add(new SortDescription(sortBy, newDir));
         }
 
-        // Methode für das auswählen eines Spiels
+        /// Methode für das auswählen eines Spiels
         private void Game_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            // Ausgwähltes Spiel wird in der variable SelectedGame speichern
+            /// Ausgwähltes Spiel wird in der variable SelectedGame speichern
             Game SelectedGame = (Game)ListViewGame.SelectedItem;
 
-            // Die if-Anweisung kann nur ausgeführt werden wenn ein Spiel ausgewählt ist
+            /// Die if-Anweisung kann nur ausgeführt werden wenn ein Spiel ausgewählt ist
             if (SelectedGame != null)
             {
-                // Labels anzeigen lassen
+                /// Labels, ScrollViewer und Buttons anzeigen lassen
                 labelName.Visibility = Visibility.Visible;
                 labelGenre.Visibility = Visibility.Visible;
                 labelPlatform.Visibility = Visibility.Visible;
                 labelReleaseDate.Visibility = Visibility.Visible;
                 labelRatingScore.Visibility = Visibility.Visible;
                 labelDescription.Visibility = Visibility.Visible;
+                scrollViewerDescription.Visibility = Visibility.Visible;
+                deleteButton.Visibility = Visibility.Visible;
 
-                // Hier wird das Cover des Spiels als Bitmap eingelesen und geladen
+                /// Hier wird das Cover des Spiels als Bitmap eingelesen und geladen
                 var bitmapImage = new BitmapImage();
                 bitmapImage.BeginInit();
 
-                // Wenn das Spiel kein Cover enthält, dann wird ein Bild mit "No Cover Available" angezeigt
+                /// Wenn das Spiel kein Cover enthält, dann wird ein Bild mit "No Cover Available" angezeigt
                 if (SelectedGame.Cover_Path.Length != 0 && SelectedGame.Cover_Path != null)
                     bitmapImage.UriSource = new Uri(SelectedGame.Cover_Path);
-                 else
-                    bitmapImage.UriSource = new Uri("https://upload.wikimedia.org/wikipedia/commons/b/b9/No_Cover.jpg");
-                
+                else
+                    bitmapImage.UriSource = new Uri("https:///upload.wikimedia.org/wikipedia/commons/b/b9/No_Cover.jpg");
+
                 bitmapImage.EndInit();
                 img.Source = bitmapImage;
 
-                // Eigenschaften des jeweiligen Spiels werden übergeben
+                /// Eigenschaften des jeweiligen Spiels werden übergeben
                 textBoxName.Text = SelectedGame.Name;
                 textBoxGenre.Text = SelectedGame.Genre;
                 textBoxPlatform.Text = SelectedGame.Platform;
                 textRelease_Date.Text = SelectedGame.Release_Date;
                 textBoxRating_Score.Text = SelectedGame.Rating_Score.ToString();
-                textBlockDescription.Text =  Encoding.Default.GetString(Encoding.Default.GetBytes(SelectedGame.Description));
+                textBlockDescription.Text = Encoding.Default.GetString(Encoding.Default.GetBytes(SelectedGame.Description));
             }
         }
 
-        // Filter-Methode für Suche, Genre, Plattform und Release-Jahr
+        /// Filter-Methode für Suche, Genre, Plattform und Release-Jahr
         private bool Filter(object item)
         {
-            // Die jeweiligen Filter werden hier in Strings umgewandelt
+            /// Die jeweiligen Filter werden hier in Strings umgewandelt
             string genreString;
             if (genreFilter.SelectedValue == null || genreFilter.SelectedValue.ToString().Length <= 36)
                 genreString = "";
@@ -137,14 +136,14 @@ namespace Games_Library
             else
                 releaseYear = releaseYearFilter.SelectedValue.ToString().Substring(38);
 
-            // Hier wird die Spieleliste mit den angewendeteten Filter zurückgegeben
+            /// Hier wird die Spieleliste mit den angewendeteten Filter zurückgegeben
             return ((item as Game).Name.IndexOf(searchFilter.Text, StringComparison.OrdinalIgnoreCase) >= 0 &&
                     (item as Game).Genre.IndexOf(genreString, StringComparison.OrdinalIgnoreCase) >= 0 &&
                     (item as Game).Platform.IndexOf(platformString, StringComparison.OrdinalIgnoreCase) >= 0 &&
                     (item as Game).Release_Date.IndexOf(releaseYear, StringComparison.OrdinalIgnoreCase) >= 0);
         }
 
-        // Text -und SelectionsHandler für die jeweiligen Filter:
+        /// Text -und SelectionsHandler für die jeweiligen Filter:
         private void searchFilter_TextChanged(object sender, TextChangedEventArgs e)
         {
             CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(ListViewGame.ItemsSource);
@@ -166,20 +165,31 @@ namespace Games_Library
             view.Filter = Filter;
         }
 
-        // ResetFilter-Methode um alle Filter wieder zuürckzusetzen und ButtonEventHandler
+        /// Methode zur Erstellung der Release-Jahre für die ComboBox
+        private void CreateReleaseYearList(int startYear, int endYear)
+        {
+            for (int i = endYear; i > startYear - 1; i--)
+            {
+                ComboBoxItem item = new ComboBoxItem();
+                item.Content = i;
+                releaseYearFilter.Items.Add(item);
+            }
+        }
+
+        /// ResetFilter-Methode um alle Filter und Sortierungen wieder zuürckzusetzen und ButtonEventHandler
         private bool ResetFilter(object item)
         {
-            // Alle Filter werden auf ihren Standartwert zurückgesetzt
+            /// Alle Filter werden auf ihren Standartwert zurückgesetzt
             searchFilter.Text = "";
             genreFilter.SelectedIndex = -1;
             platformFilter.SelectedIndex = -1;
             releaseYearFilter.SelectedIndex = -1;
 
-            // Sortierung wird zurückgesetzt
+            /// Sortierung wird zurückgesetzt
             AdornerLayer.GetAdornerLayer(listViewSortCol).Remove(listViewSortAdorner);
             ListViewGame.Items.SortDescriptions.Clear();
 
-            // Danach wird die gesamte Spieleliste einfach augegeben
+            /// Danach wird die gesamte Spieleliste einfach augegeben
             return true;
         }
         private void resetFilterButton_Click(object sender, RoutedEventArgs e)
@@ -187,51 +197,58 @@ namespace Games_Library
             CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(ListViewGame.ItemsSource);
             view.Filter = ResetFilter;
         }
-        
-        // create -und cancel List ButtonEventHandleer
-        private void createListButton_Click(object sender, RoutedEventArgs e)
+
+        /// createList, create -und cancel List ButtonEventHandleer
+        private void createLibraryButton_Click(object sender, RoutedEventArgs e)
         {
-            // InputBox wird angezeigt
+            /// InputBox wird angezeigt
             InputBox.Visibility = Visibility.Visible;
         }
-        private void createButton_Click(object sender, RoutedEventArgs e)
+        private void saveLibraryButton_Click(object sender, RoutedEventArgs e)
         {
-            // InputBox wird wieder geschlossen und die .csv-Datei wird erzeugt
+            /// InputBox wird wieder geschlossen und die .csv-Datei wird erzeugt
             InputBox.Visibility = Visibility.Collapsed;
             CreateCSVFile(InputTextBox.Text);
         }
-        private void cancelButton_Click(object sender, RoutedEventArgs e)
+        private void cancelLibraryButton_Click(object sender, RoutedEventArgs e)
         {
-            // InputBox wird wieder geschlossen und das Eingabefeld wird leer gesetzt
+            /// InputBox wird wieder geschlossen und das Eingabefeld wird leer gesetzt
             InputBox.Visibility = Visibility.Collapsed;
             InputTextBox.Text = String.Empty;
         }
 
-        // .csv-Erstell-Methode
-        public void CreateCSVFile(string fileName)
+        /// Lädt einer der gewählten Listen
+        private bool LoadLibrary(string libraryFileName)
+        {
+            ListViewGame.ItemsSource = ReadCSV(@"C:\Users\Rebin\source\repos\Games-Library\Games-Library\database/" + libraryFileName + ".csv");
+            return true;
+        }
+
+        /// Erstellt eine neue .csv-Datei
+        private void CreateCSVFile(string fileName)
         {
             try
             {
-                // .csv-Datei wird mit dem gewünschten Namen erstellt
+                /// .csv-Datei wird mit dem gewünschten Namen erstellt
                 string csvpath = @"C:\Users\Rebin\source\repos\Games-Library\Games-Library\database/" + fileName + ".csv";
                 File.AppendAllText(csvpath, "");
 
-                // InputBox wird wieder geleert
+                /// InputBox wird wieder geleert
                 InputTextBox.Text = String.Empty;
 
                 MessageBox.Show("Creating was successful.");
 
-                // Neu erstellte Liste wird der ComboBox hinzugefügt
-                librariesSelection.Items.Add(fileName);
+                /// Neu erstellte Liste wird der ComboBox hinzugefügt
+                librariesSelection.Items.Add(fileName + ".csv");
             }
-            catch 
+            catch
             {
                 MessageBox.Show("Error! Please try again.");
             }
         }
-        
-        // Lädt alle .csv-Listen als ComboBox items
-        public void CreateComboBoxListItems()
+
+        /// Lädt alle .csv-Listen in die ComboBox als ComboBoxItems
+        private void CreateComboBoxListItems()
         {
             string[] filePaths = Directory.GetFiles(@"C:\Users\Rebin\source\repos\Games-Library\Games-Library\database\", "*.csv");
             foreach (string file in filePaths)
@@ -240,13 +257,7 @@ namespace Games_Library
             }
         }
 
-        //Lädt einer der gwählten Listen
-        private bool LoadLibrary(string libraryFileName)
-        {
-            ListViewGame.ItemsSource = ReadCSV(@"C:\Users\Rebin\source\repos\Games-Library\Games-Library\database/" + libraryFileName + ".csv");
-            return true;
-        }
-        // SelectionsHandler für die Auswahl der Listen
+        /// SelectionsHandler für die Auswahl der Listen
         private void librariesSelection_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             if (librariesSelection != null)
@@ -255,6 +266,66 @@ namespace Games_Library
                 string libraryFileName = libraryFile.Remove(libraryFile.Length - 4, 4);
                 LoadLibrary(libraryFileName);
             }
+        }
+
+        /// Methode zum Löschen eines Spiels
+        private void deleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            Game SelectedGame = (Game)ListViewGame.SelectedItem;
+            string libraryFile = librariesSelection.SelectedItem.ToString();
+            string libraryFileName = libraryFile.Remove(libraryFile.Length - 4, 4);
+
+            /// Excel-Instanz wird erstellt:
+            Excel.Application excel = new Excel.Application();
+            /// Excel-Datei öffnen
+            Excel.Workbook sheet = excel.Workbooks.Open(@"C:\Users\Rebin\source\repos\Games-Library\Games-Library\database/" + libraryFile);
+            /// Arbeitsblatt wird ausgewählt
+            Excel.Worksheet x = excel.ActiveSheet as Excel.Worksheet;
+            /// Range wird erstellt
+            Excel.Range userRange = x.UsedRange;
+
+            /// Range iterieren
+            for (int i = 1; i < userRange.Rows.Count + 1; i++)
+            {
+                /// Einzelne Zelle wird eingelesen
+                string cel = ((Excel.Range)x.Cells[i, 1]).Value2.ToString();
+
+                /// Überprüft die Übereinstimmung der Daten des Spiels
+                if (cel.Contains(SelectedGame.Name + ";" + SelectedGame.Genre + ";" + SelectedGame.Platform + ";"))
+                {
+                    /// Löscht nach Übereinstimmung die Zelle
+                    x.Cells[i, 1].Delete(Excel.XlDeleteShiftDirection.xlShiftUp);
+
+                    /// Arbeitsblarr wird gespeichert
+                    sheet.Save();
+
+                    MessageBox.Show("Game has been deleted successfully");
+                }
+            }
+            /// Arbeitsblatt wird geschlossen
+            sheet.Close();
+
+            /// Spieleliste wird aktualisiert
+            LoadLibrary(libraryFileName);
+
+            /// Alle Daten in der Benutzeroberfläche werden zurückgesetzt
+            img.Source = null;
+            textBoxName.Text = "";
+            textBoxGenre.Text = "";
+            textBoxPlatform.Text = "";
+            textRelease_Date.Text = "";
+            textBoxRating_Score.Text = "";
+            textBlockDescription.Text = Encoding.Default.GetString(Encoding.Default.GetBytes(""));
+
+            /// Labels, ScrollViewer und Buttons werden wieder ausgeblendet, da kein Spiel mehr ausgwählt ist
+            labelName.Visibility = Visibility.Hidden;
+            labelGenre.Visibility = Visibility.Hidden;
+            labelPlatform.Visibility = Visibility.Hidden;
+            labelReleaseDate.Visibility = Visibility.Hidden;
+            labelRatingScore.Visibility = Visibility.Hidden;
+            labelDescription.Visibility = Visibility.Hidden;
+            scrollViewerDescription.Visibility = Visibility.Hidden;
+            deleteButton.Visibility = Visibility.Hidden;
         }
     }
 }
